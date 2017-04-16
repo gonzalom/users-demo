ee7eaf61e34decd53e9f9a0c998d9b10226baa69
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-04-16 21:18:32 +0100

    Add extra fields to the registration and profile forms

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,23 +4,177 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="`users`")
+ * @@ORM\UniqueEntity(fields="email", message="Email is already in use")
+ * @ORM\HasLifecycleCallbacks
  */
 class User extends BaseUser
 {
     /**
+     * @var integer
+     *
      * @ORM\Id
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", name="user_id", options={"unsigned":true})
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="name", length=255)
+     * @Assert\NotNull(message = "Name is missing", groups={"Registration", "Profile"})
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="smallint", name="age", options={"unsigned":true}, nullable=false)
+     * @Assert\NotNull(message = "Age is missing", groups={"Registration", "Profile"})
+     * @Assert\Range(min=1, max=150, groups={"Registration", "Profile"})
+     */
+    protected $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="job_title", length=255, nullable=false)
+     * @Assert\NotNull(message = "Job title is missing", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $jobTitle;
+
+    /**
+     * Row adding date in format ISO-8601 YYYY-MM-DDThh:mm:ss±hhmm
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", name="inserted_on", nullable=false)
+     */
+    protected $createdAt;
+
+    /**
+     * Row adding date in format ISO-8601 YYYY-MM-DDThh:mm:ss±hhmm
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", name="last_updated", nullable=true)
+     */
+    protected $updatedAt;
+
+    /**
+     * User constructor.
+     */
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getJobTitle()
+    {
+        return $this->jobTitle;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setJobTitle($jobTitle)
+    {
+        $this->jobTitle = $jobTitle;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @link http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/events.html#lifecycle-callbacks
+     */
+    public function beforePersist()
+    {
+        $this->createdAt = new \DateTime(date('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @ORM\PreUpdate
+     * @link http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/events.html#lifecycle-callbacks
+     */
+    public function beforeUpdate()
+    {
+        $this->updatedAt = new \DateTime(date('Y-m-d H:i:s'));
     }
 }
---
+++ src/AppBundle/Form/ProfileType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $current_password = $builder->get('current_password');
+
+        $builder
+            ->remove('current_password')
+            ->add('name', null, [])
+            ->add('age')
+            ->add('jobTitle')
+            ->add($current_password)
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
